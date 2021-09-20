939f96c25693a1d84e1841555b188a3078cda0a3
makssen <maks.sen.1903@mail.ru>
2021-09-20 23:43:37 +0300

    reafctoring: users api

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 require('dotenv').config()
 const mongoose = require('mongoose');
-const router = require('./router');
 const fileUpload = require('express-fileupload');
 const postRouter = require('./routes/postRouter');
 const userRouter = require('./routes/userRouter');
